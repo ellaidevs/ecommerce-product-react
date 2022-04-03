5d2fee922cf8bf9f26a8e479dce3d862bc2fd498
ellai <ellaidevs@gmail.com>
2022-04-03 15:32:06 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Section from './components/Section'
 import Poc from './components/Poc'
 
 function App() {
+  // TODO: Add overlay product-img view when clicked on product-img. https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_overlay
   return (
     <div className="App">
       {/* <Poc /> */}
