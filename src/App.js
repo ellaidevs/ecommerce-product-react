8a488f8492bf2770d65559847afd03d0aabaa3c9
ellai <ellaidevs@gmail.com>
2022-04-03 22:10:45 +0800

    add overlay div at root parent

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.scss'
 import NavBar from './components/NavBar'
 import Section from './components/Section'
 import Poc from './components/Poc'
+import { useState } from 'react'
 
 function App() {
+  let [toggleOverlay, setToggleOverlay] = useState(false)
   // TODO: Add overlay product-img view when clicked on product-img. https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_overlay
   // TODO: After finish overlay and other section area related work,  merge desktop-section-popup to page-section
+  const handleOverlayToggle = () => {
+    setToggleOverlay((toggleOverlay = !toggleOverlay))
+  }
   return (
     <div className="App">
-      {/* <Poc /> */}
+      {toggleOverlay && (
+        <div className="overlay" onClick={handleOverlayToggle}>
+          testtest
+        </div>
+      )}
       <NavBar />
-      <Section />
+      <Section setToggleOverlay={setToggleOverlay} />
     </div>
   )
 }
