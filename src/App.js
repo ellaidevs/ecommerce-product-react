6cf06f5fa0f2e667d84232b71aa05e55b9b24a3b
ellai <ellaidevs@gmail.com>
2022-04-03 15:34:18 +0800

    add todo 3th april - section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Poc from './components/Poc'
 
 function App() {
   // TODO: Add overlay product-img view when clicked on product-img. https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_overlay
+  // TODO: After finish overlay and other section area related work,  merge desktop-section-popup to page-section
   return (
     <div className="App">
       {/* <Poc /> */}
