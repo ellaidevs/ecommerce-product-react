03a748708a74e8d1de77647bd5c76ca9e58bc6e4
ellai <ellaidevs@gmail.com>
2022-04-16 23:35:30 +0800

    simplify showAddToCart onclick function

--- src/components/NavbarRight.jsx
+++ src/components/NavbarRight.jsx
@@ -3,17 +3,17 @@ import { ReactComponent as IconCart } from '../assets/icon-cart.svg'
 import Avatar from '../assets/image-avatar.png'
 
 const NavbarRight = ({ sumOfCart }) => {
-  const showAddToCart = (x) => {
-    console.log('checking showAddToCart', sumOfCart)
+  const cartValue = sumOfCart.sumOfCart
+
+  const showAddToCart = () => {
+    console.log('checking showAddToCart', cartValue)
   }
 
   return (
     <div className="navbar-right">
       <div className="svg-cart">
-        {sumOfCart.sumOfCart > 0 && (
-          <div className="cart-value">{sumOfCart.sumOfCart}</div>
-        )}
-        <IconCart onClick={() => showAddToCart('test')} className="icon-cart" />
+        {cartValue > 0 && <div className="cart-value">{cartValue}</div>}
+        <IconCart onClick={showAddToCart} className="icon-cart" />
       </div>
       <div className="avatar">
         <img src={Avatar} alt="profile" />
