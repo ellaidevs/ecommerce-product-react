1c40051e5853cf7b89c72735c2c859f3fd3f8bc5
ellai <ellaidevs@gmail.com>
2022-04-16 23:32:44 +0800

    conditionally render add-to-cart value when the value is more than zero

--- src/components/NavbarRight.jsx
+++ src/components/NavbarRight.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { ReactComponent as IconCart } from '../assets/icon-cart.svg'
 import Avatar from '../assets/image-avatar.png'
 
-const NavbarRight = (sumOfCart) => {
+const NavbarRight = ({ sumOfCart }) => {
   const showAddToCart = (x) => {
     console.log('checking showAddToCart', sumOfCart)
   }
+
   return (
     <div className="navbar-right">
       <div className="svg-cart">
-        <div className="cart-value">0</div>
+        {sumOfCart.sumOfCart > 0 && (
+          <div className="cart-value">{sumOfCart.sumOfCart}</div>
+        )}
         <IconCart onClick={() => showAddToCart('test')} className="icon-cart" />
       </div>
       <div className="avatar">
