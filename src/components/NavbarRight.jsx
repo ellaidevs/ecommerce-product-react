cda77dd84fedfc0505ffc2add9df205e07d7bd18
ellai <ellaidevs@gmail.com>
2022-04-16 12:42:24 +0800

    show add-to-cart value

--- src/components/NavbarRight.jsx
+++ src/components/NavbarRight.jsx
@@ -3,10 +3,14 @@ import { ReactComponent as IconCart } from '../assets/icon-cart.svg'
 import Avatar from '../assets/image-avatar.png'
 
 const NavbarRight = () => {
+  const showAddToCart = (x) => {
+    console.log('checking showAddToCart', x)
+  }
   return (
     <div className="navbar-right">
       <div className="svg-cart">
-        <IconCart className="icon-cart" />
+        <div className="cart-value">3</div>
+        <IconCart onClick={() => showAddToCart('test')} className="icon-cart" />
       </div>
       <div className="avatar">
         <img src={Avatar} alt="profile" />
