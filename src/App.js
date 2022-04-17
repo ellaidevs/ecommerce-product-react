0195de588b8d2b34d6fe5b706c44d387072663d5
ellai <ellaidevs@gmail.com>
2022-04-17 23:42:58 +0800

    add todos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let [toggleOverlay, setToggleOverlay] = useState(false)
   let [sumOfCart, setSumOfCart] = useState(0)
   // Todo: Bring (sumOfCart) data from RightSection -> Section -> App(root) to -> NavBar -> NavbarRight -> AddToCart -> CartContent //This state is being passed via many layers of components. Also, this state is value is updated in two-way direction. The developer have decided to make use of Redux for this case to reduce complexity state management.
+  // Todo: apply redux for this case
   return (
     <div className="App">
       <OverlayProduct
