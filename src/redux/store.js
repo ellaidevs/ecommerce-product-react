d5bb4114879753ea2e4f54499846a06ad202f79d
ellai <ellaidevs@gmail.com>
2022-04-26 22:07:16 +0800

    include cart reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter'
+import cartReducer from './cart'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    cart: cartReducer,
   },
 })
