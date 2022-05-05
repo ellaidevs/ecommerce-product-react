fc90f036c4742d843df58af0e0c83ed529ac66d6
ellai <ellaidevs@gmail.com>
2022-05-06 01:17:29 +0800

    fix scss specificity

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Categories from './Categories'
 import NavbarRight from './NavbarRight'
-
+import { ReactComponent as HamburgerMenu } from '../assets/icon-menu.svg'
 const NavBar = () => {
   return (
     <div className="nav">
       <div className="container">
+        <HamburgerMenu className="hamburger-menu" />
         <h1 className="nav-title">sneakers</h1>
         <Categories />
         <NavbarRight />
