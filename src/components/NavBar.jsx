961ae3fc1f040416e865b426c60da4080b56fc97
ellai <ellaidevs@gmail.com>
2022-05-06 01:24:22 +0800

    add onclick for side bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,13 @@ import Categories from './Categories'
 import NavbarRight from './NavbarRight'
 import { ReactComponent as HamburgerMenu } from '../assets/icon-menu.svg'
 const NavBar = () => {
+  const handleSidebar = () => {
+    console.log('side bar clicked')
+  }
   return (
     <div className="nav">
       <div className="container">
-        <HamburgerMenu className="hamburger-menu" />
+        <HamburgerMenu onClick={handleSidebar} className="hamburger-menu" />
         <h1 className="nav-title">sneakers</h1>
         <Categories />
         <NavbarRight />
