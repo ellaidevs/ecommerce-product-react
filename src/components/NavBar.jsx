d3d35c15988d2a7179dacd51f4ba78a5c771116d
ellai <ellaidevs@gmail.com>
2022-05-07 01:29:27 +0800

    toggle sidebar state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Categories from './Categories'
 import NavbarRight from './NavbarRight'
 import { ReactComponent as HamburgerMenu } from '../assets/icon-menu.svg'
@@ -6,8 +6,12 @@ const NavBar = () => {
   // ! TODO 7th May 2022
   // TODO: 3. Toggle categories display when clicked on hamburger menu. 4. When the categories is displayed at mobile view, handle the scss to display the categories as sidebar.
   // TODO: Once completed mobile navbar, merge mobile-nav -> staging
+
+  let [toggleSideBar, setToggleSideBar] = useState(false)
+
   const handleSidebar = () => {
     console.log('side bar clicked')
+    setToggleSideBar(!toggleSideBar)
   }
   return (
     <div className="nav">
