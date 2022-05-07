dea848482e81ef8d62c4ca87854a75554258fd7d
ellai <ellaidevs@gmail.com>
2022-05-07 21:22:48 +0800

    dynamically render sidebar classsname

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Categories = () => {
+  const toggleSidebar = useSelector((state) => state.sidebar.value)
   return (
-    <div className="categories">
+    <div className={toggleSidebar ? 'categories mobile-sidebar' : 'categories'}>
       <h3>Collections</h3>
       <h3>Men</h3>
       <h3>Women</h3>
