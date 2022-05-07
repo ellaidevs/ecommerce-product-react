dcd71e33c9b6352dcd34cc5bf623c742f7705c0a
ellai <ellaidevs@gmail.com>
2022-05-07 21:18:24 +0800

    use sidebar redux at react sidebar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Categories from './Categories'
 import NavbarRight from './NavbarRight'
 import { ReactComponent as HamburgerMenu } from '../assets/icon-menu.svg'
+import { useSelector, useDispatch } from 'react-redux'
+import { setToggleSidebar } from '../redux/sidebar'
 const NavBar = () => {
   // ! TODO 7th May 2022
   // TODO: 3. Toggle categories display when clicked on hamburger menu. 4. When the categories is displayed at mobile view, handle the scss to display the categories as sidebar.
   // TODO: Once completed mobile navbar, merge mobile-nav -> staging
 
-  let [toggleSideBar, setToggleSideBar] = useState(false)
+  const dispatch = useDispatch()
+  const sidebarToggle = useSelector((state) => state.sidebar.value)
 
-  const handleSidebar = () => {
-    console.log('side bar clicked')
-    setToggleSideBar(!toggleSideBar)
-  }
+  console.log('sidebarToggle', sidebarToggle)
   return (
     <div className="nav">
       <div className="container">
-        <HamburgerMenu onClick={handleSidebar} className="hamburger-menu" />
+        <HamburgerMenu
+          onClick={() => dispatch(setToggleSidebar())}
+          className="hamburger-menu"
+        />
         <h1 className="nav-title">sneakers</h1>
         <Categories />
         <NavbarRight />
