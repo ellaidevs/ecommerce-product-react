8f505401399b2d211bae871fbaa5218f7c28c43f
ellai <ellaidevs@gmail.com>
2022-05-07 21:18:02 +0800

    add sidebar toggle redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter'
 import cartReducer from './cart'
 import toggleOverlay from './toggleOverlay'
+import sidebarReducer from './sidebar'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     cart: cartReducer,
     toggle: toggleOverlay,
+    sidebar: sidebarReducer,
   },
 })
