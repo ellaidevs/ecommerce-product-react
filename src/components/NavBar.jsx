1d5e16a89da6958c980f5fd18f0d6e87ff84ea96
ellai <ellaidevs@gmail.com>
2022-05-15 01:20:29 +0800

    add Todo 14th May 2022

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { setToggleSidebar } from '../redux/sidebar'
 import { setToggleMobileSideBar } from '../redux/mobileSideBar'
 const NavBar = () => {
-  // ! TODO 7th May 2022
-  // TODO: 1.Add X svg to categories. 2. Hide X button at web and position relative. 3.make other categories add margin-top when X svg appear at mobile
-  // TODO 4. Add proper style for sidebar
+  // ! TODO 14th May 2022
+  // TODO: 1. Fix margin issue of categories when mobile nav sidebar is activated.
   // TODO: Once completed mobile navbar, merge mobile-nav -> staging
 
   const dispatch = useDispatch()
