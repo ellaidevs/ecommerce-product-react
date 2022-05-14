a79a640bfdcd42176100fc87fa1eb7ebc3405c0a
ellai <ellaidevs@gmail.com>
2022-05-14 22:48:41 +0800

    fire toggle mobilesider when click hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import NavbarRight from './NavbarRight'
 import { ReactComponent as HamburgerMenu } from '../assets/icon-menu.svg'
 import { useSelector, useDispatch } from 'react-redux'
 import { setToggleSidebar } from '../redux/sidebar'
+import { setToggleMobileSideBar } from '../redux/mobileSideBar'
 const NavBar = () => {
   // ! TODO 7th May 2022
   // TODO: 1.Add X svg to categories. 2. Hide X button at web and position relative. 3.make other categories add margin-top when X svg appear at mobile
@@ -13,14 +14,14 @@ const NavBar = () => {
   const dispatch = useDispatch()
   const sidebarToggle = useSelector((state) => state.sidebar.value)
 
-  console.log('sidebarToggle', sidebarToggle)
+  const fireHamburgerMenu = () => {
+    dispatch(setToggleSidebar())
+    dispatch(setToggleMobileSideBar())
+  }
   return (
     <div className="nav">
       <div className="container">
-        <HamburgerMenu
-          onClick={() => dispatch(setToggleSidebar())}
-          className="hamburger-menu"
-        />
+        <HamburgerMenu onClick={fireHamburgerMenu} className="hamburger-menu" />
         <h1 className="nav-title">sneakers</h1>
         <Categories />
         <NavbarRight />
