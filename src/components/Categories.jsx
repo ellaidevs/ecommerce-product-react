99e529c278f2fe7120d384548b43aa93738e25b3
ellai <ellaidevs@gmail.com>
2022-05-14 22:08:51 +0800

    add position and spacing style to mobile nav

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { ReactComponent as CancelIcon } from '../assets/icon-close.svg'
 
 const Categories = () => {
   const toggleSidebar = useSelector((state) => state.sidebar.value)
   return (
     <div className={toggleSidebar ? 'categories' : 'hide-categories'}>
+      <CancelIcon className="nav-close-icon" />
       <h3>Collections</h3>
       <h3>Men</h3>
       <h3>Women</h3>
