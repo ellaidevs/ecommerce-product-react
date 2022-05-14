37b65550f0a7372fc3ec2774723385a780885725
ellai <ellaidevs@gmail.com>
2022-05-14 22:21:04 +0800

    code split categories into Category component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { ReactComponent as CancelIcon } from '../assets/icon-close.svg'
+import Category from './Category'
 
 const Categories = () => {
   const toggleSidebar = useSelector((state) => state.sidebar.value)
   return (
     <div className={toggleSidebar ? 'categories' : 'hide-categories'}>
-      <CancelIcon className="nav-close-icon" />
-      <h3>Collections</h3>
-      <h3>Men</h3>
-      <h3>Women</h3>
-      <h3>About</h3>
-      <h3>Contact</h3>
+      <Category />
     </div>
   )
 }
