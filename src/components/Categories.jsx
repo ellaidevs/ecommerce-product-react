dc75bca27d5a44eff9df7c6746174b5199df0405
ellai <ellaidevs@gmail.com>
2022-05-19 19:33:45 +0800

    remove unused useSelector

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,11 +4,9 @@ import Category from './Category'
 
 const Categories = () => {
   const toggleSidebar = useSelector((state) => state.sidebar.value)
-  const mobileSideBar = useSelector((state) => state.mobileSideBar.value)
 
-  console.log('what is is mobile sidebar', mobileSideBar)
   return (
-    <div className={toggleSidebar ? 'categories' : 'hide-categories'}>
+    <div className={toggleSidebar ? 'sidebar-active' : 'hide-categories'}>
       <Category />
     </div>
   )
