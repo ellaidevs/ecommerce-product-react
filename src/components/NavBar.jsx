d27a31e464bc76b36a2fd49451950fc4337773e8
ellai <ellaidevs@gmail.com>
2022-05-19 19:35:07 +0800

    remove 14th may 2022 todo & code cleanup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,11 @@ import React from 'react'
 import Categories from './Categories'
 import NavbarRight from './NavbarRight'
 import { ReactComponent as HamburgerMenu } from '../assets/icon-menu.svg'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { setToggleSidebar } from '../redux/sidebar'
 import { setToggleMobileSideBar } from '../redux/mobileSideBar'
 const NavBar = () => {
-  // ! TODO 14th May 2022
-  // TODO: 1. Fix margin issue of categories when mobile nav sidebar is activated.
-  // TODO: Once completed mobile navbar, merge mobile-nav -> staging
-
   const dispatch = useDispatch()
-  const sidebarToggle = useSelector((state) => state.sidebar.value)
 
   const fireHamburgerMenu = () => {
     dispatch(setToggleSidebar())
