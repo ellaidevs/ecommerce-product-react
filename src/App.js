9b5710d9a8c9279bc00bcf40802d439eeb66bd2e
ellai <ellaidevs@gmail.com>
2022-09-25 23:58:56 +0800

    chore: add todo 25th september

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss'
 import NavBar from './components/NavBar'
 import Section from './components/Section'
 import OverlayProduct from './components/OverlayProduct'
-
+// Todo: please add sidebar mobile view to staging, must go through some conflict 
 function App() {
   return (
     <div className="App">
